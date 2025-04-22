5f648ae60d638d05d5311b4427d4cd1f7670338f
loganpauley <ltpauley@outlook.com>
2025-04-22 10:32:13 -0400

    Fix ReactFlow provider and dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './app'
 import './index.css'
 import 'reactflow/dist/style.css'
-import { ReactFlowProvider } from 'reactflow'
+import { ReactFlowProvider } from '@reactflow/core'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
