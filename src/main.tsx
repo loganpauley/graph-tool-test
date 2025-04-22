229810751a85b7058074f5500cfdb8151ed07228
loganpauley <ltpauley@outlook.com>
2025-04-22 10:29:50 -0400

    Add ReactFlowProvider to fix white screen

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './app'
 import './index.css'
 import 'reactflow/dist/style.css'
+import { ReactFlowProvider } from 'reactflow'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ReactFlowProvider>
+      <App />
+    </ReactFlowProvider>
   </React.StrictMode>
 )
